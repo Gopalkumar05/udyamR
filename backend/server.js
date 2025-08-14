67cf68c321d9daf07e47bcaeb7f6e47254e804fd
Gopalkumar05 <112787134+Gopalkumar05@users.noreply.github.com>
2025-08-14 18:57:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,7 @@ const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors(
-  {origin:"https://udyamr.onrender.com"}
-));
+app.use(cors());
 app.use(express.json());
 
 // Generate OTP endpoint
