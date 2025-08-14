0de4b6c3a171a2ffb6bdd070aa2e5da9f06b622b
Gopalkumar05 <roy639300@gmail.com>
2025-08-14 18:17:29 +0530

    Fix Prisma for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {origin:"https://udyamr.onrender.com"}
+));
 app.use(express.json());
 
 // Generate OTP endpoint
